3c4558a25bc2dff2f0f239968612881df18c4ccc
Gordon Soutter <gordon.soutter@outlook.com>
2020-05-25 17:52:04 +0100

    adds new WaterBottle class, and getVolume method

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+
+    WaterBottle bottle;
+
+    @Before
+    public void before(){
+        bottle = new WaterBottle();
+    }
+
+    @Test
+    public void getVolume(){
+        assertEquals(100, bottle.getVolume(), 0.1);
+    }
+
+
+}
