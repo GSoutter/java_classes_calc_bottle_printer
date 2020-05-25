6120c64edecd7474702add6e6f1bd8b4f631acd6
Gordon Soutter <gordon.soutter@outlook.com>
2020-05-25 17:37:05 +0100

    adds subtract function to calculator. with test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -10,4 +10,20 @@ public class CalculatorTest {
     public void canAdd(){
         assertEquals(2, calculator.add(1,1), 0.01);
     }
+
+    @Test
+    public void canAddDecimal(){
+        assertEquals(3000000.3333, calculator.add(1000000.1111,2000000.2222), 0.01);
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(1, calculator.subtract(2,1), 0.01);
+    }
+
+
+    @Test
+    public void canSubtractDecimal(){
+        assertEquals(3000000.3333, calculator.subtract(5000000.5555,2000000.2222), 0.01);
+    }
 }
